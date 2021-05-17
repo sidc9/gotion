ea314b42f3bf69d75f59488f51ca4d50fd36a106
Siddhanta <sid.choco@gmail.com>
2021-05-17 22:50:19 +0800

    Fixed db prop

--- properties.go
+++ properties.go
@@ -3,8 +3,8 @@ package gotion
 type DatabaseProperties map[string]*DatabaseProperty
 
 type DatabaseProperty struct {
-	Name string `json:"-"`
-	// Title       []Title                `json:"title"`
+	Name        string                 `json:"-"`
+	Title       Title                  `json:"title"`
 	ID          string                 `json:"id"`
 	Type        string                 `json:"type"`
 	Number      DBPropNumber           `json:"number"`
